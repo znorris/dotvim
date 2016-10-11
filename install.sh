215df1b5e4c7095d4e9b35e88d7e4dda00895b43
Zach Norris <znorris@gmail.com>
2016-10-10 17:00:41 -0700

    More robust setup script.

--- install.sh
+++ install.sh
@@ -1,16 +1,49 @@
 #!/bin/bash
+set -ex
 
-git clone http://github.com/joshfriend/dotvim.git ~/.vim
+# Test if we have python3 install in vim
+# this check isn't good enough
+# if ! vim --version |grep -w '+python3'; then
+#   echo "Go install vim with python3 support first."
+#   exit 1
+# fi
 
+if [ -d ~/.vim ]; then
+  echo "The ~/.vim directory exists. Proceeding will remove this directory."
+  read -p "Are you sure? (y / N) " -n 1 -r
+  echo    # (optional) move to a new line
+  if [[ $REPLY =~ ^[Yy]$ ]]
+    then
+      continue
+  else
+    exit 0
+  fi
+fi
+
+basedir=~/.vim/dotvim
+
+# Git our sweet repo
+git clone git@github.com:znorris/dotvim.git ${basedir}
+
+vimfile=~/.vimrc
+gvimfile=~/.gvimrc
 # Back up old vim config files
-mv ~/.vimrc ~/.vimrc.bak
-mv ~/.gvimrc ~/.gvimrc.bak
+if [ -f $vimfile ]; then
+  mv ${vimfile} ~/.vimrc.bak
+fi
+if [ -f $gvimfile ]; then
+  mv ${gvimfile} ~/.gvimrc.bak
+fi
 
 # Set new config files
-ln -s ~/.vim/vimrc ~/.vimrc
-ln -s ~/.vim/gvimrc ~/.gvimrc
+ln -s ${basedir}/vimrc ~/.vimrc
+ln -s ${basedir}/gvimrc ~/.gvimrc
 
 # Get plugins
 cd ~/.vim
 git submodule update --init
+# Install plugins
+vim +PluginInstall +qall
 
+# Install YouCompleteMe
+~/.vim/bundle/youcompleteme/install.py
